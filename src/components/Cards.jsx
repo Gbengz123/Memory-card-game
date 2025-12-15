17f7c7c68b1f4a134223265342ff07beefdadade
Danny <danny@Daniels-MacBook-Air.local>
2025-12-15 16:45:00 +0100

    Add shuffle render

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Card from './Card';
+
+function shuffleCharacters(characterArr) {
+  const arr = [...characterArr]; // avoid mutation
+  // Loop and shuffle array
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
 
 function Cards() {
   const [characters, setCharacters] = useState([]);
+  const shuffledCharacters = useRef(null);
+  // fetch cahracter data on mount
   useEffect(() => {
     async function getCharacters() {
       try {
@@ -10,7 +22,13 @@ function Cards() {
         if (!res.ok) throw new Error(`HTTP Error: ${res.status}`);
 
         const data = await res.json();
-        setCharacters(data.characters);
+        // prevent rerender of shuffled characters
+        if (shuffledCharacters.current) {
+          setCharacters(shuffledCharacters.current);
+        } else {
+          shuffledCharacters.current = shuffleCharacters(data.characters);
+          setCharacters(shuffledCharacters.current);
+        }
       } catch (error) {
         console.error(`$Request failed: ${error}`);
       }
