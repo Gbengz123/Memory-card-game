807344016312578b37ffe88b3239b75867553793
Danny <danny@Daniels-MacBook-Air.local>
2025-12-15 17:16:01 +0100

    Add click event for reshuffle render

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,10 @@
-function Card({ character }) {
+function Card({ character, shuffleCharacters, characters, setCharacters }) {
   return (
-    <div className="flex cursor-pointer flex-col items-center gap-10 rounded-2xl p-3 shadow-lg">
-      <img src={character.images[0]} alt={character.name} className="h-52 w-52 border" />
+    <div
+      onClick={() => setCharacters(shuffleCharacters(characters))}
+      className="flex cursor-pointer flex-col items-center gap-6 rounded-2xl p-3 shadow-lg"
+    >
+      <img src={character.images[0]} alt={character.name} className="h-52" />
       <p className="text-2xl">{character.name}</p>
     </div>
   );
